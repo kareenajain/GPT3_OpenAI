98be1f37568b9247d09f137fd16a33c124da585e
Kareena Jain <kareena1jain@gmail.com>
2023-07-14 15:14:57 +0530

    added new feature

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -18,7 +18,7 @@ const featuresData = [
   {
     title:'Really boy law county',
     text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.'
-  }
+  },
 ]
 
 const Features = () => {
